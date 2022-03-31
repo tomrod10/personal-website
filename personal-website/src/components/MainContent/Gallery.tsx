894bb6c3fd335d66bc2f537e15540b78df7d4b7b
Tomas Rodriguez <tomasrod09@hotmail.com>
2022-03-31 11:36:01 -0700

    fixed scroll issue

--- personal-website/src/components/MainContent/Gallery.tsx
+++ personal-website/src/components/MainContent/Gallery.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { dummyData } from "../../core-data/dummy-data.ts";
+import 'Any weight_Any size_5th try_4300.jpg' from ../../core-data/dummy-data.ts
 
 import styles from "./MainContent.module.css"
 
 export const Gallery = () => {
   return (
     <div className={styles.gallery}>
-      gallery
+      {dummyData.map((item) => (
+        <div className={styles.item}>{item}</div>
+        // <img></img>
+      ))}
     </div>
   )
 }
