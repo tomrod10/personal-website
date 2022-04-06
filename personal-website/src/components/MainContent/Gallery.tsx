e1bb1e4c66156ca75b899453eafb1a9b1bc3970b
Tomas Rodriguez <tomasrod09@hotmail.com>
2022-04-05 21:35:54 -0700

    WIP

--- personal-website/src/components/MainContent/Gallery.tsx
+++ personal-website/src/components/MainContent/Gallery.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { dummyData } from "../../core-data/dummy-data.ts";
-import 'Any weight_Any size_5th try_4300.jpg' from ../../core-data/dummy-data.ts
 
 import styles from "./MainContent.module.css"
 
@@ -9,7 +8,6 @@ export const Gallery = () => {
     <div className={styles.gallery}>
       {dummyData.map((item) => (
         <div className={styles.item}>{item}</div>
-        // <img></img>
       ))}
     </div>
   )
