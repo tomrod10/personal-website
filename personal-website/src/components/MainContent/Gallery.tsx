1de882bf8b963738c310a137c1d062cd3fcdf8a8
Tomas Rodriguez <tomasrod09@hotmail.com>
2022-04-12 00:50:33 -0700

    progress babyyyy

--- personal-website/src/components/MainContent/Gallery.tsx
+++ personal-website/src/components/MainContent/Gallery.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { dummyData } from "../../core-data/dummy-data.ts";
+import { images } from "../../images"
 
 import styles from "./MainContent.module.css"
 
 export const Gallery = () => {
   return (
     <div className={styles.gallery}>
-      {dummyData.map((item) => (
-        <div className={styles.item}>{item}</div>
+      {images.map((image) => (
+        <img src={image.img} className={styles.item} alt={image.title}/>
       ))}
     </div>
   )
---
+++ personal-website/src/images/index.js
@@ -0,0 +1,20 @@
+export const images = [
+  {
+    title: 'Hang in there',
+    date: '10/09/2019',
+    description: 'lorem ipsum blah blah',
+    img: require('./Poster No. 6_Winner.jpg'),
+  },
+  {
+    title: 'Mt. Baldy',
+    date: '10/10/2019',
+    description: 'lorem ipsum blah blah',
+    img:  require('./Poster No. 7_Winner.jpg'),
+  },
+  {
+    title: 'Sling and stone',
+    date: '10/11/2019',
+    description: 'lorem ipsum blah blah',
+    img: require('./Poster No. 8_Winner.jpg'),
+  },
+]
