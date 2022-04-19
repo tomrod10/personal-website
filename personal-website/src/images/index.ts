f81f782eae50c7935d4e758d9ed38c5e51ed56ed
Tomas Rodriguez <tomasrod09@hotmail.com>
2022-04-19 00:31:16 -0700

    added two extra fields to image object for future use

--- personal-website/src/images/index.ts
+++ personal-website/src/images/index.ts
@@ -4,6 +4,8 @@ export const images: [] = [
       date: '10/09/2019',
       description: 'lorem ipsum blah blah',
       img: ('./Poster No. 6_Winner.jpg'),
+      author: 'Tomás Rodríguez',
+      category: ['all, design'], // subject to change depending the links for the navbar
     },
     // update the src paths to the rest of the images!
     {
@@ -11,29 +13,39 @@ export const images: [] = [
       date: '10/10/2019',
       description: 'lorem ipsum blah blah',
       img:  ('./Poster No. 7_Winner.jpg'),
+      author: 'Tomás Rodríguez',
+      category: ['all, design'],
     },
     {
       title: 'Sling and stone',
       date: '10/11/2019',
       description: 'lorem ipsum blah blah',
       img: ('./Poster No. 8_Winner.jpg'),
+      author: 'Tomás Rodríguez',
+      category: ['all, design'],
     },
     {
       title: 'Autocorrect',
       date: '10/14/2019',
       description: 'lorem ipsum blah blah',
       img: ('./Poster No. 11_Winner.jpg'),
+      author: 'Tomás Rodríguez',
+      category: ['all, design'],
     },
     {
       title: 'Permanent Beta',
       date: '10/21/2019',
       description: 'lorem ipsum blah blah',
       img: ('./Poster No. 18_Winner.jpg'),
+      author: 'Tomás Rodríguez',
+      category: ['all, design'],
     },
     {
       title: 'Razer',
       date: '10/24/2019',
       description: 'lorem ipsum blah blah',
       img: ('./Poster No. 21_Bonus_Freebie Winner.jpg'),
+      author: 'Tomás Rodríguez',
+      category: ['all, design'],
     },
   ]
