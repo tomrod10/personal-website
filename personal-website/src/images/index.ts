fa9b8996f3b3c3eae862ee29c016457ca8b8fbf1
Tomas Rodriguez <tomasrod09@hotmail.com>
2022-04-18 22:50:24 -0700

    update images and gallery files

--- personal-website/src/images/index.ts
+++ personal-website/src/images/index.ts
@@ -1,38 +1,39 @@
-export const images = [
+export const images: [] = [
   {
     title: 'Hang in there',
     date: '10/09/2019',
     description: 'lorem ipsum blah blah',
-    img: require('./Poster No. 6_Winner.jpg'),
+    img: ('./images/Poster No. 6_Winner.jpg'),
   },
+  // update the src paths to the rest of the images!
   {
     title: 'Mt. Baldy',
     date: '10/10/2019',
     description: 'lorem ipsum blah blah',
-    img:  require('./Poster No. 7_Winner.jpg'),
+    img:  ('./Poster No. 7_Winner.jpg'),
   },
   {
     title: 'Sling and stone',
     date: '10/11/2019',
     description: 'lorem ipsum blah blah',
-    img: require('./Poster No. 8_Winner.jpg'),
+    img: ('./Poster No. 8_Winner.jpg'),
   },
   {
     title: 'Autocorrect',
     date: '10/14/2019',
     description: 'lorem ipsum blah blah',
-    img: require('./Poster No. 11_Winner.jpg'),
+    img: ('./Poster No. 11_Winner.jpg'),
   },
   {
     title: 'Permanent Beta',
     date: '10/21/2019',
     description: 'lorem ipsum blah blah',
-    img: require('./Poster No. 18_Winner.jpg'),
+    img: ('./Poster No. 18_Winner.jpg'),
   },
   {
     title: 'Razer',
     date: '10/24/2019',
     description: 'lorem ipsum blah blah',
-    img: require('./Poster No. 21_Bonus_Freebie Winner.jpg'),
+    img: ('./Poster No. 21_Bonus_Freebie Winner.jpg'),
   },
 ]
