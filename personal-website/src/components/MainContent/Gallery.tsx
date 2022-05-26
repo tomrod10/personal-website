1cb226b0b5f0dccf3a602081002d7480d7622965
Tomas Rodriguez <tomasrod09@hotmail.com>
2022-05-26 01:13:56 -0700

    add view switch btns

--- personal-website/src/components/MainContent/Gallery.tsx
+++ personal-website/src/components/MainContent/Gallery.tsx
@@ -14,21 +14,26 @@ interface Image {
   categories: string[],
 }
 
-
-export const Gallery = () => {
+export const Gallery = ({ isGridView }) => {
   const category = useParams().all // don't know why it's all under 'all' ???
   const [filter, setFilter] = useState('all')
   useEffect(() => {
     setFilter(category)
   }, [category])
 
-  const filtered: Image[] = (images.filter(((image) => image.categories.includes(filter))))
+  const filtered: Image[] = (images.filter(((image: Image) => image.categories.includes(filter))))
 
-  return (
-    <div className={styles.gallery}>
-      {filtered.map((image: Image) => {
-        return <img src={image.img} className={styles.item} alt={image.title} key={image.img}/>
-      })}
-    </div>
+  return(
+    !isGridView
+    ?
+      <div className={styles.gallery}>
+        {filtered.map((image: Image) => {
+          return <img src={image.img} className={styles.item} alt={image.title} key={image.img}/>
+        })}
+      </div>
+    :
+      <div>
+        grid view
+      </div>
   )
 }
--- personal-website/src/components/MainContent/SideBar.tsx
+++ personal-website/src/components/MainContent/SideBar.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 
+import gridIcon2 from "../../icons/svg-icons/gridIcon2.svg";
+import solidSquare from "../../icons/svg-icons/solidSquare.svg";
+
 import styles from "./MainContent.module.css"
 
-export const SideBar = () => {
+
+export const SideBar = ({ view, onViewSwitch }) => {
+  const handleGalleryView = (view: boolean) => {
+    onViewSwitch(view)
+  }
+
   return (
     <div className={styles.sidebar}>
-      sidebar
+      <div className={styles.icon} onClick={() => handleGalleryView(false)}>
+        <img src={solidSquare} alt="Column view" />
+      </div>
+      <div className={styles.icon} onClick={() => handleGalleryView(true)}>
+        <img src={gridIcon2} alt="Grid view" />
+      </div>
     </div>
   )
 }
